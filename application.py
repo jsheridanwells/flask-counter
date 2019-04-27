7b35dba983bf217a10c130ece8a329080d91e74a
Jeremy Wells <jsheridanwells@gmail.com>
2019-04-27 15:13:12 -0500

    unit tests

--- application.py
+++ application.py
@@ -7,9 +7,12 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(**config_overrides):
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+
+    app.config.update(config_overrides)
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
