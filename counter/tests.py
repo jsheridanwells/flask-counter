9f5d8d0aff4dd6872b473618afb1499579142113
Jeremy Wells <jsheridanwells@gmail.com>
2019-04-28 08:18:42 -0500

    fixed test

--- counter/tests.py
+++ counter/tests.py
@@ -14,11 +14,11 @@ from util.test_db import TestDB
 class CounterTest(unittest.TestCase):
     def create_app(self):
         return create_app_base(
-            SQL_ALCHEMY_DATABASE_URI=self.db_uri,
+            SQLALCHEMY_DATABASE_URI=self.db_uri,
             TESTING=True,
-            SECRET_KEY='mySecret'
+            SECRET_KEY = 'mySecret!'
         )
-    
+
     def setUp(self):
         self.test_db = TestDB()
         self.db_uri = self.test_db.create_db()
@@ -26,12 +26,12 @@ class CounterTest(unittest.TestCase):
         self.app = self.app_factory.test_client()
         with self.app_factory.app_context():
             db.create_all()
-    
+
     def tearDown(self):
         with self.app_factory.app_context():
             db.drop_all()
         self.test_db.drop_db()
-    
+
     def test_counter(self):
         rv = self.app.get('/')
         assert '1' in str(rv.data)
